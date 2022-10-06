f0910989f9fe148f8601eb56e999aa9175e9a8ca
wwdyKawinpat <kawinpat.kit@gmail.com>
2022-10-06 12:23:25 +0700

    MajorChange , AddComment

--- Quiz4.py
+++ Quiz4.py
@@ -1,22 +1,50 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jul 21 11:48:00 2022
 
-@author: Win11
-"""
+#Retrieved Logic Statement from use
+statement = str(input("Logic Statement :"))
+statement = statement.lower()                       # Convert x into lower case form.
+# Create Table Header
+print ('|-----------Truth Table----------|')
 
-x = str(input("Statement :"))
-a = False
-b = True
+def f(a,b):                         # Create function to evaluate 'AND' 'OR' 'NOT'
+    return eval(statement)
+                                    # Manually create other opertions function
+def NAND (a,b):                     # like 'NAND' 'NOR' 'XOR' 'NXOR'
+    if a == True and b == True :
+        return(False)
+    else :
+        return(True)
 
-def f(a,b):
-    return eval(x)
+def NOR (a,b):
+    if a == False and b == False :
+        return(True)
+    else :
+        return(False)
 
-print(f)
-print("----------------------")
-print("------TruthTable------")
-
-for a in [b, a]:
-    for b in [b, a]:
-        print(str(a),str(b), str(f(a,b)))
-        a
+def XOR (a,b):
+    if a == True and b == True :
+        return(False)
+    elif a == False and b == False :
+        return(False)
+    else :
+        return(True)
+        
+def NXOR (a,b):
+    if a == True and b == True :
+        return(True)
+    elif a == False and b == False :
+        return(True)
+    else :
+        return(False)         
+                                    # Showing Truth Table.
+for a in [True, False]:
+    for b in [True, False]:
+        if ' nand' in statement: # Checking conditions by word.
+            print('|--{:^6}--|--{:^6}--|--{:^6}--|'.format(str(a),str(b), str(NAND (a,b)))) 
+        elif 'nor' in statement:
+            print('|--{:^6}--|--{:^6}--|--{:^6}--|'.format(str(a),str(b),str(NOR(a,b))))
+        elif 'xor' in statement:
+            print('|--{:^6}--|--{:^6}--|--{:^6}--|'.format(str(a),str(b),str(XOR(a,b))))
+        elif 'nxor' in statement:
+            print('|--{:^6}--|--{:^6}--|--{:^6}--|'.format(str(a),str(b),str(NXOR(a,b))))
+        else:
+            print('|--{:^6}--|--{:^6}--|--{:^6}--|'.format(str(a),str(b),str(f(a,b)))) 
